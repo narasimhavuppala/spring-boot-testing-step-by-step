97809b8ffc85ede76ab4d608ab4d1447716f5472
narasimhavuppala <nraovuppala@Wgmail.com>
2018-09-13 11:05:53 +0530

    Scafolding an app

---
+++ src/main/java/com/zwayam/ContactCrud/ContactCrudApplication.java
@@ -0,0 +1,12 @@
+package com.zwayam.ContactCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactCrudApplication.class, args);
+	}
+}
